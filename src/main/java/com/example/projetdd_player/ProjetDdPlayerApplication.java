ad8e5ba076f8c206f68e9de6f4217cc004b0c34d
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-07 09:32:25 +0100

    first commit

---
+++ src/main/java/com/example/projetdd_player/ProjetDdPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetdd_player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDdPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetDdPlayerApplication.class, args);
+    }
+
+}
